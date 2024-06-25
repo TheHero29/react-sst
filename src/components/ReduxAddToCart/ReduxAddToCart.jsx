5d14a651d2c1810f18730a53c4e83665c73fffe9
TheHero29 <loukik.23bcs10059@ms.sst.scaler.com>
2024-06-26 01:30:39 +0530

    learning redux more

--- src/components/ReduxAddToCart/ReduxAddToCart.jsx
+++ src/components/ReduxAddToCart/ReduxAddToCart.jsx
@@ -2,33 +2,34 @@ import { useSelector,useDispatch } from "react";
 import { CartContext } from "../../contexts/CartContext";
 function ReduxAddToCart({product}) {
     //useSelector 
+    // how is useSelector taking state as input?
     let cart = useSelector((state)=>{
         return state.item;
     })    
     function increase(){
         //dispatch {type,payload}
-        dispatch({type:'ADD_TO_CART',payload:product})
+        dispatchEvent({type:'ADD_TO_CART',payload:product})
     }
     function decrease(){
         //dispatch {type,payload}
-        dispatch({type:'REMOVE_FROM_CART',payload:product})
+        dispatchEvent({type:'REMOVE_FROM_CART',payload:product})
     }
 
     const quantity = cart[product.id] ? cart[product.id].quantity : 0;
     if(quantity===0)
         return (
     <div>
-        <button onClick={()=>{{increaseQuantity(product)} console.log("increasing")}}>Add to cart</button>
+        <button onClick={increase}>Add to cart</button>
     </div>
     )
     else
     return (
         <div>
             {/* <button onClick={()=>{setQuantity(quantity+1)}}>+</button> */}
-            <button onClick={()=>{increaseQuantity(product)}}>+</button>
+            <button onClick={increase}>+</button>
              <span>{quantity}</span>
             {/* <button onClick={()=>{if(quantity>0)setQuantity(quantity-1)}}>-</button> */}
-            <button onClick={()=>{decreaseQuantity(product)}}>-</button>
+            <button onClick={decrease}>-</button>
         </div>
  )
 }
--- src/store.js
+++ src/store.js
@@ -1,11 +1,59 @@
 import {createStore} from 'react'
+import { omit } from 'loadash'
 function CartReducer(state = {items:{}},action){
     switch(action.type){
         case 'ADD_TO_CART':{
             const product = action.payload;
-            
+            // doesn't making a copy everytime to make a rerender instead of modifying it cause lot of memory waste
+            if(state.items[product.id])
+            {
+                return {
+                    ...state,
+                    items:{
+                        ... state.items,
+                        [product.id]:{
+                            ...state.items[product.id],
+                            quantity:state.items[product.id].quantity+1
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return{
+                    ...state,
+                    items:{
+                        ...state.items,
+                        [product.id]:{
+                            ...product,
+                            quantity:1
+                        }
+                    }
+                } 
+            }
         }
         case 'REMOVE_FROM_CART':{
+            const product = action.payload;
+            if(state.items[product.id].quantity>1)
+            {
+                return{
+                    ...state,
+                    items:{
+                        ...state.items,
+                        [product.id]:{
+                            ...product,
+                            quantity:state.items[product.id].quantity-1
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return {
+                    ...state,
+                    items: omit(state.items,[product.id])
+                }
+            }
 
         }
         default:
